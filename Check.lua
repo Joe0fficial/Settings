06513c8091355c8bd9f07ac98d57212c4338d9e3
Joex0fficial <139757196+Joe0fficial@users.noreply.github.com>
2023-09-07 02:15:38 -0700

    Create Check.lua

---
+++ Check.lua
@@ -0,0 +1,6 @@
+return {
+  ["FREE"] = true,
+  ["PAID"] = false,
+  ["FREE ERROR"] = "Free Is Disabled by the Owner. Please Contact the Owner for more Informations",
+  ["PAID ERROR"] = "Paid Is Disabled by the Owner. Please Contact the Owner for more Informations",
+}
